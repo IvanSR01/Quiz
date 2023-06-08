5fea640025c8810ed5c5a643212386efe3137350
zxcivan07 <108341880+zxcivan07@users.noreply.github.com>
2023-06-08 17:06:47 +0300

    Update

--- src/components/Item/ItemStart.tsx
+++ src/components/Item/ItemStart.tsx
@@ -1,37 +1,49 @@
 import React from 'react'
 import styles from './Item.module.scss'
-import { Link, useLocation, useParams } from 'react-router-dom';
-import Button from '../Button/Button';
-
+import { Link, useLocation, useParams } from 'react-router-dom'
+import Button from '../Button/Button'
+import { motion } from 'framer-motion'
 interface IItem {
-	title: string;
-	imgUrl?: string;
-	i: number;
-	setClick?:(num: number) => void
+	title: string
+	imgUrl?: string
+	i: number
+	setClick?: (num: number) => void
 }
 
-const ItemStart: React.FC<IItem> = ({ title, imgUrl, i, setClick}) => {
+const ItemStart: React.FC<IItem> = ({ title, imgUrl, i, setClick }) => {
 	const { pathname } = useLocation()
 	const { id } = useParams()
 	const onClick = (num: number) => {
-		return  setClick ? setClick(num) : console.log(null)
+		return setClick ? setClick(num) : console.log(null)
 	}
 	return (
-		<div className={styles.wrapper}>
-			<img src={`https://zxcivan07.github.io/Quiz/${imgUrl || ''}`} alt="" />
+		<motion.div
+			className={styles.wrapper}
+			initial={{
+				y: '100px',
+				opacity: 0
+			}}
+			whileInView={{
+				y: '0px',
+				opacity: 1
+			}}
+			transition={{
+				delay: 0,
+				duration: 0.5
+			}}
+		>
+			<img src={`https://zxcivan07.github.io/Quiz/${imgUrl || ''}`} alt='' />
 			<p>{title}</p>
-			{pathname === `/quiz/${id}` ?
+			{pathname === `/quiz/${id}` ? (
 				<>
-					<Button onClick={() => onClick(1)}>
-						Приступить
-					</Button>
-				</> :
+					<Button onClick={() => onClick(1)}>Приступить</Button>
+				</>
+			) : (
 				<Link to={`/quiz/${i}`}>
-					<Button>
-						Приступить
-					</Button>
-				</Link>}
-		</div>
+					<Button>Приступить</Button>
+				</Link>
+			)}
+		</motion.div>
 	)
 }
 
