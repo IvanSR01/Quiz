6a613745c07f2ef39fa5833132ed7c52c322ad22
zxcivan07 <108341880+zxcivan07@users.noreply.github.com>
2023-06-08 14:40:55 +0300

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import SearchProvider from './provider/SearchProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<BrowserRouter>
+	<HashRouter>
 		<SearchProvider>
 			<App />
 		</SearchProvider>
-	</BrowserRouter>
+	</HashRouter>
 )
